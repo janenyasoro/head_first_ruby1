e73d4b8f906d2cd55dab3e2b627d43d8f9cf0571
janenyasoro <janenyasoro@gmail.com>
2016-10-04 12:29:17 +0300

    a program that uses attribute writer and reader

---
+++ 02_chapter2/student.rb
@@ -0,0 +1,31 @@
+class Student
+	def age= (age)
+		@age = age
+	end
+	def age
+		@age
+		puts "#{@age}"
+	end
+	def grade= (grade)
+		@grade = grade
+	end
+	def grade
+		@grade
+		puts "#{@grade}"
+	end
+	def form= (form)
+		@form = form
+	end
+	def form
+		@form
+		puts "#{@form}"
+	end
+end
+jane = Student.new
+jane.age=15
+jane.grade= "B"
+jane.form= 1
+
+jane.age
+jane.grade
+jane.form
